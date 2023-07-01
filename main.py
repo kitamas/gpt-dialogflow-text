a7f4c18c38add3fc00c92a3a5dba0f115a3e8f22
kitamas <kitamas25@gmail.com>
2023-07-01 09:00:41 +0200

    commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import flask
+import json
+import os
+from flask import send_from_directory, request
+
+# Flask app should start in global layout
+app = flask.Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/favicon.png')
+
+@app.route('/')
+
+@app.route('/home')
+def home():
+    return "Hello World"
+
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    req = request.get_json(force=True)
+    print(req)
+    text = "webhook text response"
+
+    #return {
+    #    "fulfillmentText": "webhook text response"
+    #}
+
+    res = {
+        "fulfillment_response": {"messages": [{"text": {"text": [text]}}]}
+    }
+
+    return res
+
+if __name__ == "__main__":
+#    app.debug = True
+    app.run()
