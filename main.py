8bfd87957b7f8f25a38f7ac903a1f434fc624a33
kitamas <kitamas25@gmail.com>
2023-07-04 18:19:04 +0200

    commit

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def retrieve(query_text):
     ]
     # print("CONTEXTS = ",contexts)
     # limit = 3750 TIMEOUT ??
-    limit = 750
+    limit = 3750
 
     # build our prompt with the retrieved contexts included
     prompt_start = (
